735932be5ce7b91f2e8847b64d66f8dea315f874
Avdhesh Kumar <avdheshkumar@Avdheshs-MacBook-Pro.local>
2020-04-21 19:31:13 +0530

    read port from env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express.io');
 var app = express();
 app.http().io();
-var PORT = 5000;
+var PORT = process.env.PORT || 3000;
 console.log('server started on port ' + PORT);
 
 app.use(express.static(__dirname + '/public'));
