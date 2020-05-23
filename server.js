2b570be0ea65de91a41d2d5ab0dda5d1413f43a5
Avdhesh Kumar <avdheshkumar@Avdheshs-MacBook-Pro.local>
2020-05-23 16:43:26 +0530

    added video call

--- server.js
+++ server.js
@@ -10,6 +10,40 @@ app.get('/', function(req, res){
     //console.log("serving request..."+ JSON.stringify(req))
     res.render('index.ejs');
 });
+app.get('/videocall', function(req, res){
+    //console.log("serving request..."+ JSON.stringify(req))
+    res.render('videocall.ejs');
+});
+
+// var mime = {
+//     html: 'text/html',
+//     txt: 'text/plain',
+//     css: 'text/css',
+//     gif: 'image/gif',
+//     jpg: 'image/jpeg',
+//     png: 'image/png',
+//     svg: 'image/svg+xml',
+//     js: 'application/javascript'
+// };
+//
+// app.get('/public/*', function (req, res) {
+//     // var file = path.join(dir, req.path.replace(/\/$/, '/index.html'));
+//     // if (file.indexOf(dir + path.sep) !== 0) {
+//     //     return res.status(403).end('Forbidden');
+//     // }
+//     var type = mime[path.extname(file).slice(1)] || 'text/plain';
+//     var s = fs.createReadStream(file);
+//     s.on('open', function () {
+//         res.set('Content-Type', type);
+//         s.pipe(res);
+//     });
+//     s.on('error', function () {
+//         res.set('Content-Type', 'text/plain');
+//         res.status(404).end('Not found');
+//     });
+// });
+
+
 
 app.listen(PORT);
 
